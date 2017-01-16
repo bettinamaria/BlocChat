3737175bac62dfb7f2db6f81a4e0bcbe4d75071f
bettinamaria <bettina_offenegger@hotmail.com>
2017-01-16 18:25:24 +0100

    user story 1 listChatRooms: edited

--- app/scripts/factories/Room.js
+++ app/scripts/factories/Room.js
@@ -1,7 +1,10 @@
 (function() {
   function Room($firebaseArray) {
     var ref = firebase.database().ref().child("rooms");
+    ref.orderByChild().endAt('room3');
+    console.log(ref);
     var rooms = $firebaseArray(ref);
+    console.log(ref);
       
       
     return {
--- dist/scripts/controllers/HomeCtrl.js
+++ dist/scripts/controllers/HomeCtrl.js
@@ -1,14 +1,26 @@
 /**
-* @desc Controller created to display Rooms in the view
-* @type {Object}?? or param {xx}??
+* @desc Controller created to display Rooms in the view, associated with home template through $state. 
 */
 (function() {
+     function config($locationProvider, $stateProvider) {
+         $locationProvider
+            .html5Mode({
+                enabled: true,
+                requireBase: false
+            });
+         $stateProvider
+             .state('home', {
+                 url: '/',
+                 controller: 'HomeCtrl as home',
+                 templateUrl: '/templates/home.html'
+             });
+
      function HomeCtrl(Room, $scope) {
-            $scope.rooms = Room.all;
+            $scope.rooms = $firebaseArray(ref);
      }
  
      angular
          .module('blocChat')
          .controller('HomeCtrl', ['Room', '$scope', HomeCtrl]);
- })();
-
+     }
+})();
