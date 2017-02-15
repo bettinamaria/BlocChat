e54b72d1854221128d1d8a015c7f6548c2d93296
bettinamaria <bettina_offenegger@hotmail.com>
2017-02-15 23:53:31 +0100

    user-story-3-listMessages addedFirebasedataforRoomChatMessages and embedded JS code

--- dist/scripts/factories/Message.js
+++ dist/scripts/factories/Message.js
@@ -1,6 +1,20 @@
 (function() {
   function Message($firebaseArray) {
     var ref = firebase.database().ref().child("messages");
+    var messages = $firebaseArray(ref);
+      
+    return {
+        send: function(newMessage, roomId){
+                messages.$add({
+                    Bettina: newMessage,
+                    roomId: roomID,
+                    sentAt: timestamp
+                });
+        }
+        getByRoomId: function (roomId) {
+        messages.orderByChild('roomId').isEqual(ref);
+        }
+    };
   }
 
   angular
